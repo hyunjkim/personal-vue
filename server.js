3d129d8a1008862880ca0c4ef85c231b09d91014
hyunjkim <hkim6501@gmail.com>
2018-08-28 19:03:06 -0400

    update heroku

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(express.static(join(__dirname, '/dist')));
 app.use(express.static(resolve(__dirname, '.', 'public'))); // Serve static files from ../public
 app.get('*/', (_, res) => res.sendFile(resolve(__dirname, '.', 'public', 'index.html'))); // Send index.html for any other requests.
 
-app.listen(port, () => console.log("starting server: ", port));
+app.listen(port, () => console.log("Starting server: ", port));
